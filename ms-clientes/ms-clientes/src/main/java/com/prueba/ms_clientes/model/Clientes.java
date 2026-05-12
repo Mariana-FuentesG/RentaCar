b3ddb7a79d2d67aac16ee1428672863228681e26
Mariana-FuentesG <maria.fuentesg@duocuc.cl>
2026-05-12 19:50:18 -0400

    PrimerCommit

---
+++ ms-clientes/ms-clientes/src/main/java/com/prueba/ms_clientes/model/Clientes.java
@@ -0,0 +1,12 @@
+package com.prueba.ms_clientes.model;
+
+import java.time.LocalDate;
+
+public class Clientes {
+    private Integer id;
+    private String nombre;
+    private String email;
+    private Integer telefono;
+    private Boolean activo;
+    private LocalDate fechaRegistro;
+}
